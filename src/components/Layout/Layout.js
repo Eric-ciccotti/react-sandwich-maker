357e3e2475f00849bcda730d804d1d6cc9a57f00
Eric-ciccotti <eric.ciccotti@gmail.com>
2020-03-18 01:38:16 +0100

    Layout.module.css added, implementation of BurgerBuilder Container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 
 const Layout = (props) => (
     <Aux>
         <div>ToolBar, SideDrawer (tiroir latéral), Backdrop (toile de fond)</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
