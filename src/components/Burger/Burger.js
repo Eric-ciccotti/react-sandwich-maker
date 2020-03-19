726fced8c4db1934b55014c2ebd8b4291a14060d
Eric-ciccotti <eric.ciccotti@gmail.com>
2020-03-20 00:40:09 +0100

    Refactored transformedIngredients function in Burger.js ( more readable and easy to understand )

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,19 +6,15 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
 
     //goal : transform ingredients props.object into an array of burger ingredients components
-    //---------------------------------------------------------------------------------------------
-    //Object.keys() = To get objects keys from 'props.ingredients' and return an 'array of strings' 
-    //on this array I execute a map() = it create an [] with inside many empty array depending ingredient amount 
-    //on this array i execute map() = I'm only interested by the key (_,i) so, I return 
-    //<BurgerIngredient /> component with key et type
-    
-    const transformedIngredients = Object.keys(props.ingredients)
-    .map((ingredientKey) => {
-        return[...Array(props.ingredients[ingredientKey])].map((_,i) => {
-            return <BurgerIngredient type={ingredientKey} key={ingredientKey + i} />
-        });
-    });
-    
+
+    let transformedIngredients = [];
+
+    for (let key in props.ingredients) {
+        for (let i = 0; i < props.ingredients[key]; i++) {
+            transformedIngredients.push(<BurgerIngredient key={key + i} type={key} />);
+        }
+    }
+
 
     return (
         <div className={classes.Burger}>
@@ -31,3 +27,15 @@ const burger = (props) => {
 
 
 export default burger;
+
+  //Object.keys() = To get objects keys from 'props.ingredients' and return an 'array of strings' 
+    //on this array I execute a map() = it create an [] with inside many empty array depending ingredient amount 
+    //on this array i execute map() = I'm only interested by the key (_,i) so, I return 
+    //<BurgerIngredient /> component with key et type
+    //---------------------------------------------------------------------------------------------
+    // const transformedIngredients = Object.keys(props.ingredients)
+    //     .map((ingredientKey) => {
+    //         return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+    //             return <BurgerIngredient type={ingredientKey} key={ingredientKey + i} />
+    //         });
+    //     });
