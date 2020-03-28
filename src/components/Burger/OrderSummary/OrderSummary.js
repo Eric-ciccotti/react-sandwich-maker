5b75bc6b1f2f700a9372867f34b40e32ba51a057
Eric-ciccotti <eric.ciccotti@gmail.com>
2020-03-28 16:19:13 +0100

    button reusable component which enable making button without new css and so on

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Auxiliary';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
         const ingredientSummary = Object.keys(props.ingredients)
@@ -31,8 +32,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to CheckOut ?</p>
-            <button>CANCEL</button>
-            <button>CONTINUE</button>
+            <Button btnType="Danger" clicked={props.cancel}>Cancel</Button>
+            <Button btnType="Success" clicked={props.continue}>Continue</Button>
         </Aux>
     );
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,6 +83,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false})
     }
 
+    purschaseContinueHandler = () => {
+        alert('Continue the order ;)');
+    }
+
     render() {
         /* DISABLE BUTTON HANDLER */
         const disabledInfo = {
@@ -101,7 +105,10 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                    <OrderSummary 
+                        ingredients={this.state.ingredients} 
+                        cancel={this.purchaseCancelHandler}
+                        continue={this.purschaseContinueHandler}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
