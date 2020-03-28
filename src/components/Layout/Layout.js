b895d3a8d6cee8039d59bbec9127fddf5078f3da
Eric-ciccotti <eric.ciccotti@gmail.com>
2020-03-28 17:02:08 +0100

    Added Navbar component, which is displayed in the Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import Toolbar from '../../components/Navigation/Toolbar';
 import classes from './Layout.module.css';
+
 
 const Layout = (props) => (
     <Aux>
-        <div>ToolBar, SideDrawer (tiroir latéral), Backdrop (toile de fond)</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
