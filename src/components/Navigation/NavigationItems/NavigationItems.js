9909bda21c489dec63f76c2b8c7955629d39dfee
Eric-ciccotti <eric.ciccotti@gmail.com>
2020-03-29 21:50:27 +0200

    Navigation Items with inside Navigation Item component, linked to the toolbar component

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import classes from './NavigationItems.module.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const NavigationItems = () => (
+        <ul className={classes.NavigationItems}>
+            <NavigationItem link="/" active>Burger Builder</NavigationItem>
+            <NavigationItem link="/">Checkout</NavigationItem>
+        </ul>
+);
+
+
+export default NavigationItems;
+
+// même chose :
+// <NavigationItem link="/" active={true}>Burger Builder</NavigationItem>
+// <NavigationItem link="/" active>Burger Builder</NavigationItem>
+
